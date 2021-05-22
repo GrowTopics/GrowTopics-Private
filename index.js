58f4df40b3d2ea9e952ee39740f53f02ca8869a4
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-21 18:56:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const prefix = "[Your Prefix]"
+const prefix = "="
 
 // THIS IS THE STATUS
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("[your custom status]", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
-        type: "STREAMING", // THIS IS THE STATUS TYPE
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
+    bot.user.setActivity("GrowTopics", {
+        type: "STREAMING",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 
 }
 )
-
-// THIS IS THE ([prefix]help) COMMAND
-
-// [SCRIPT GOES HERE]
 
 // THIS IS THE bot.login
 
