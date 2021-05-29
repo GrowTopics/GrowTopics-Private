6c3d56b22e022f946c86f4e95272630581f21b5d
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-29 08:57:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const prefix = "="
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
     
-    bot.channels.cache.get('847602473627025448').send(`<:Green:845805015108812810> Bot Online`)
+    bot.channels.cache.get('847602473627025448').send(`🟢 Bot Online`)
     
 	bot.user.setActivity(`www.growtopics.xyz`, {
         type: "WATCHING",
