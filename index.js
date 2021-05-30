f6b414ce1e369f700d966210a5bc1cd04fe850d0
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-30 08:21:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ bot.on('message', async message => {
             userID = userID.replace(/[^0-9]/g, '');
 
             const member = tdc.members.cache.get(userID);
-            const content = args.join(' ').replace(`${userID} `, '')
+            const content = args.join(' ').replace(`<@ ${userID} > `, '')
 
             if (!member) return message.channel.send('Unable to find that user');
 
