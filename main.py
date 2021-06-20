e9b5c4e9ed57b9faf91d07cceb3be2a8ac8ac83b
ChickenDevs <chickenthef1sh@gmail.com>
2021-06-20 16:19:59 -0600

    Should get the bot online

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hippity hoppity yeet that javascript off my property")
+import discord
+import os
+from discord.ext.commands import Bot, when_mentioned_or
+
+
+def get_prefix(bot, message):
+    return when_mentioned_or(*["="])(bot, message)
+
+
+class GrowTopicsPrivate(Bot):
+    def __init__(self):
+        super().__init__(
+            get_prefix, 
+            help_command=None, 
+            description="GrowTopics Private Bot",
+            activity=discord.Game("loading..."),
+            case_insensitive=True,
+            intents=discord.Intents.all()
+        )
+        
+
+GrowTopicsPrivate().run(os.getenv("token"))
