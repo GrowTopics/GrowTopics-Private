ad3bd2469b30f4cbeb4fe5cba4bf63cdef364575
ChickenDevs <chickenthef1sh@gmail.com>
2021-06-20 16:28:13 -0600

    Add eval

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from discord.ext.commands import Bot, when_mentioned_or
 
 
+COGS = ['jishaku']
+
+
 def get_prefix(bot, message):
     return when_mentioned_or(*["="])(bot, message)
 
@@ -17,6 +20,23 @@ class GrowTopicsPrivate(Bot):
             case_insensitive=True,
             intents=discord.Intents.all()
         )
+        
+        for cog in COGS:
+            self.load_extension(cog)
+
+    def load_extension(self, name, *, package):
+        try:
+            super().load_extension(name, package=package)
+            print(f"Loaded {name}")
+        except Exception as e:
+            print(f"Couldn't load {name} - {e}")
+            
+    def unload_extension(self, name, *, package):
+        try:
+            super().unload_extension(name, package=package)
+            print(f"Unloaded {name}")
+        except Exception as e:
+            print(f"Couldn't unload {name} - {e}")
         
 
 GrowTopicsPrivate().run(os.getenv("token"))
