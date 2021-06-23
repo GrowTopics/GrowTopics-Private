e74cf1a31f8919992f188e272fe7a17ed6fbd35b
ChickenDevs <chickenthef1sh@gmail.com>
2021-06-22 20:31:25 -0600

    make it match the old message

--- cogs/owner.py
+++ cogs/owner.py
@@ -16,6 +16,9 @@ class Owner(Cog):
     
     @command("dm")
     async def _dm(self, ctx, user: discord.User, *, msg):
-        await user.send(msg)
+        e = discord.Embed(title=f"{ctx.author.name} has sent you a message!", description=msg)
+        e.set_thumbnail(ctx.author.avatar_url)
+        e.set_footer(text=f"User ID: {ctx.author.id}")
+        await user.send(embed=e)
         await ctx.reply(f"📫 Sent to {user.mention}.")
     
