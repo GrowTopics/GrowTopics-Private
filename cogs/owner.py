90d17ee60c8b025ffd3966100db5b731f81bb081
TheReaper62 <34223146+TheReaper62@users.noreply.github.com>
2021-07-07 16:54:53 +0800

    DM Command Edit + Listener

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,20 +5,34 @@ from discord.ext.commands import Cog, command
 
 def setup(bot):
     bot.add_cog(Owner(bot))
-    
+
 
 class Owner(Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
     async def cog_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
     
     @command("dm")
     async def _dm(self, ctx, user: discord.User, *, msg):
-        e = discord.Embed(title=f"{ctx.author.name} has sent you a message!", description=msg, color=discord.Color(0x14242C))
-        e.set_thumbnail(url=ctx.author.avatar_url)
-        e.set_footer(text=f"User ID: {ctx.author.id}")
-        await user.send(embed=e)
-        await ctx.reply(f"📫 Sent to {user.mention}.")
-    
+        if ctx.author.permissions_in(ctx.channel).manage_messages:
+            if content == None:
+                await ctx.send("No Message Content Given!!!")
+                await ctx.send("Command Format should be: `=dm <user_identifier> <message>` ",delete_after=5)
+                return
+            try:
+                e = discord.Embed(title=f"{ctx.author.name} has sent you a message!", description=msg, color=discord.Color(0x14242C))
+                e.set_thumbnail(url=ctx.author.avatar_url)
+                e.set_footer(text=f"User ID: {ctx.author.id}")
+                await user.send(embed=e)
+                await ctx.reply(f"📫 **Direct Message** has been sent to {user.name}.")
+            except Exception as err:
+                await ctx.send(f"An Error Occured while sending a DM:\n{err}")
+        else:
+            await ctx.send("Only staff members can use this command.")
+
+    @_dm.error
+    async def invalid_usererror(ctx, err:commands.CommandError):
+        if isinstance(err, commands.BadArgument):
+            return await ctx.send('Invalid User Identifier or user could not be found')
--- main.py
+++ main.py
@@ -21,16 +21,16 @@ def get_prefix(bot, message):
 class GrowTopicsPrivate(Bot):
     def __init__(self):
         super().__init__(
-            get_prefix, 
-            help_command=None, 
+            get_prefix,
+            help_command=None,
             description="GrowTopics Private Bot",
             activity=discord.Game("loading..."),
             case_insensitive=True,
             intents=discord.Intents.all()
         )
-        
+
         self.log = logger
-        
+
         for cog in COGS:
             self.load_extension(cog)
 
@@ -40,36 +40,48 @@ class GrowTopicsPrivate(Bot):
             self.log.info(f"Loaded {name}")
         except Exception as e:
             self.log.error(f"Couldn't load {name} - {e}")
-            
+
     def unload_extension(self, name, *, package=None):
         try:
             super().unload_extension(name, package=package)
             self.log.info(f"Unloaded {name}")
         except Exception as e:
             self.log.error(f"Couldn't unload {name} - {e}")
-            
+
     async def on_message(self, m):
         if m.author.bot:
             return
-        
         ctx = await self.get_context(m)
         if ctx.valid:
             return await self.process_commands(m)
-        
+
+        # DM Listener
+        if m.guild == None:
+            content = m.content
+            e = discord.Embed(
+                title = f"{m.author.name} sent us a message!",
+                description = f"From {m.author.mention}\n{content}",
+                colour = discord.Colour(0x14242C)
+            )
+            e.set_thumbnail(url=m.author.avatar_url)
+            e.set_footer(text=f"User ID: {m.author.id}")
+            await self.get_channel(845476323005956116).send(embed=e)
+
+            return
     async def on_ready(self):
         self.log.info("Ready!")
         await self.change_presence(activity=discord.Activity(name="www.growtopics.xyz", type=discord.ActivityType.watching))
-        
+
     async def close(self):
         await super().close()
         self.log.info("Shutdown Complete.")
-        
+
     def run(self):
         try:
             self.log.info("Starting...")
             super().run(os.getenv("token"), reconnect=True)
         except Exception as e:
             self.log.critical(f"Could not start - {e}")
-        
+
 
 GrowTopicsPrivate().run()
